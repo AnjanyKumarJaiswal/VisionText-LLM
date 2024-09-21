48f7388dc49842a2024de2a892660a950a1fcd23
AnjanyKumarJaiswal <136046942+AnjanyKumarJaiswal@users.noreply.github.com>
2024-09-22 00:27:18 +0530

    fac: ready for deployment

---
+++ src/streamlit.py
@@ -0,0 +1,42 @@
+import streamlit as st
+from models.embeddings import MultiModalEmbedder
+from models.gemini import GenerateGeminiResponse
+from utils.pineconedb import PineConeVectorDB
+from dotenv import load_dotenv
+import os
+from PIL import Image
+
+load_dotenv()
+
+embedder = MultiModalEmbedder()
+pinecone_instances = None  
+
+st.title("Multimodal RAG Application")
+
+uploaded_file = st.file_uploader("Upload a PDF file", type=["pdf"])
+
+generate_button = st.button("Generate Answer")
+
+if uploaded_file:
+    try:
+        # Create a Pinecone instance with the uploaded PDF file
+        pinecone_instances = PineConeVectorDB(embedder=embedder, pdf_path=uploaded_file)
+        pinecone_instances.connect()
+        pinecone_instances.upsert_embedding()
+
+        gemini = GenerateGeminiResponse(
+            Gemini_API_KEY=os.getenv('GEMINI_API_KEY'),
+            pinecone=pinecone_instances,
+            Embedder=embedder,
+            model='models/gemini-pro'
+        )
+
+        user_query = st.text_input("Ask me anything!", "")
+        if generate_button:
+            if user_query:
+                response = gemini.generate_query(user_query)
+                st.text(response)
+            else:
+                st.warning("Please enter a question.")
+    except Exception as e:
+        st.error(f"An error occurred: {e}")
